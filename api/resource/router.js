050f0a540bbcfe8cb8c1060169f95011f62cfb1f
c_melchor <cah.melchor@gmail.com>
2021-01-22 07:57:38 -0800

    getALL functions working for all projects,tasks, resources

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,15 @@
 // build your `/api/resources` router here
+const express = require("express");
+const router = express.Router();
+const Resources = require("./model");
+
+router.get("/", async (req, res) => {
+  try {
+    const getResources = await Resources.getAll();
+    res.status(200).json(getResources);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+});
+
+module.exports = router;
