2ed4b6dbcb585a9cb339927a094aa663c2d2e4b5
c_melchor <cah.melchor@gmail.com>
2021-01-21 19:09:59 -0800

    tasks table set up

--- data/migrations/20210121182502_project.js
+++ data/migrations/20210121182502_project.js
@@ -1,13 +1,28 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("projects", tbl => {
-    tbl.increments("project_id");
-    tbl.string("project_name", 128).notNullable();
-    tbl.string("project_description", 128);
-    tbl
-      .boolean("project_completed")
-      .notNullable()
-      .defaultTo(false);
-  });
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments("project_id");
+      tbl.string("project_name", 128).notNullable();
+      tbl.string("project_description", 128);
+      tbl
+        .boolean("project_completed")
+        .notNullable()
+        .defaultTo(false);
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments("tasks_id");
+      tbl.string("task_description", 128).notNullable();
+      tbl.string("task_notes", 1000);
+      tbl
+        .boolean("task_completed")
+        .notNullable()
+        .defaultTo(false);
+      tbl
+        .integer("project_id")
+        .notNullable()
+        .references("project_id")
+        .inTable("projects");
+    });
 };
 
 exports.down = function(knex) {
